35816256171599dc5cd92e71500407fbe2ae6040
KarlaSan21 <Kwessanch@gmail.com>
2021-10-27 11:09:35 -0700

    beginning code

---
+++ variablesAndDataTypes.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+my_text = "yo"
+print(my_text)
+print(my_text, "how are you")
+
+glass_of_water = 3
+print("I drank", glass_of_water, "glasses of water today")
+
+glass_of_water = glass_of_water + 1
+print(glass_of_water)
+
+
+
