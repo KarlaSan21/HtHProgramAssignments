096b61dfd085aa369c8154d9fdfad4d9392e09fe
KarlaSan21 <Kwessanch@gmail.com>
2021-10-27 11:17:48 -0700

    types

--- variablesAndDataTypes.py
+++ variablesAndDataTypes.py
@@ -1,3 +1,4 @@
+
 print("Hello World")
 
 my_text = "yo"
@@ -10,5 +11,25 @@ print("I drank", glass_of_water, "glasses of water today")
 glass_of_water = glass_of_water + 1
 print(glass_of_water)
 
+man_stepped_on_moon = 12
+print(man_stepped_on_moon)
+
+my_reason_for_coding = "I find it fun"
+print(my_reason_for_coding)
+
+global_mean_sea_level_2018 = 21.65
+print(global_mean_sea_level_2018)
+
+staying_alive = True
+print(staying_alive)
+
+answer_1 = type(int)
+answer_2 = type(str)
+answer_3 = type(float)
+answer_4 = type(bool)
+print(answer_1)
+print(answer_2)
+print(answer_3)
+print(answer_4)
 
 
