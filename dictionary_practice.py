da809e3e138a638d19772b8c607f24dc831e6b63
KarlaSan21 <Kwessanch@gmail.com>
2021-11-02 12:07:27 -0700

    part 1 done

---
+++ dictionary_practice.py
@@ -0,0 +1,18 @@
+
+prices = {
+    "chicken" : 1.59,
+    "beef" : 1.99,
+    "cheese" : 1.00,
+    "milk" : 2.50
+}
+
+friends_age = {
+    "Pedro" : 19,
+    "Mariana" : 19,
+    "Rachel" : 19,
+    "Kierra" : 19
+}
+
+
+rachel_age = friends_age["Rachel"]
+
