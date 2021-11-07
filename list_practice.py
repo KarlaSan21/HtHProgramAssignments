282ffc802c8b565a0a035fb466289c82c2ecf895
KarlaSan21 <Kwessanch@gmail.com>
2021-11-06 19:12:57 -0700

    finished

--- list_practice.py
+++ list_practice.py
@@ -1,3 +1,44 @@
 
 # step 2
+# Oakland, Atlanta, New York City, 
+# Seattle, Memphis, Miami, Boston, 
+# Los Angeles, Denver, and New Orleans
+US_cities = ["Oakland", "Atlanta", "New York City",
+"Seattle", "Memphis", "Miami", "Boston", "Los Angeles",
+"Denver", "New Orleans"]
 
+print(US_cities)
+
+# step 3
+
+print(US_cities[0], US_cities[3], US_cities[-3])
+
+# step 4
+
+four_cities = US_cities[4 : 8]
+
+print(four_cities)
+
+# step 5
+
+US_cities[0] = "San Francisco"
+US_cities[2] = "Brooklyn"
+US_cities[-3] = "Hollywood"
+US_cities[5] = "Tampa"
+
+print(US_cities)
+
+# step 6
+
+US_cities.append("Oakland")
+US_cities.extend(["New York City", "Los Angeles"])
+US_cities.insert(0, "Miami")
+
+print(US_cities)
+
+# step 6
+del US_cities[6]
+US_cities.pop(7)
+US_cities.remove("Denver")
+
+print(US_cities)
