aa38bdc2a2701b9072e5892436d7a470ff7cc771
KarlaSan21 <Kwessanch@gmail.com>
2021-11-30 10:09:55 -0800

    added error handling file

---
+++ errorhandling.py
@@ -0,0 +1,26 @@
+"""
+We're writing a program that adds two numbers together. 
+Test this and solve any errors you find if you can. 
+Solving one problem might turn up a new error...
+Be sure to keep a note of whatever errors you find!
+"""
+
+
+print("Welcome to the addition helper")
+number1 = int(input("Please enter the first number: "))
+number2 = int(input("Please enter the second number: "))
+
+print("The result is: ", number1 + number2)
+
+
+
+""""
+errors:
+    missing t in print
+    no ending quotation in first print
+    space outside of quotations in first question
+    the user inputs have to be type casted as integers
+        only concatinating the numbers together, not actually adding them together
+    number_2 instead of number2
+    
+"""
